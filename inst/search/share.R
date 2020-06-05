31207c33cbb8f4b040d9a28022a99eec0612b861
herreio <git@herre.io>
2020-06-05 21:53:45 +0200

    inst updated

--- inst/search/share.R
+++
@@ -1,9 +0,0 @@
-cat("init share\n")
-
-library(maxsim)
-
-tts <- function(query, compare, result) {
-  publications <- maxsim::search_dist_items(query, compare, result)
-  cat("done comparing query to publications!\n")
-  return(publications)
-}
